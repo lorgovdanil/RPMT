3ad00a3c9340f2b32ade199c292d80612e70c7b8
lorgovdanil <lorgovdanil@gmail.com>
2024-09-03 10:45:22 +0500

    Update task2.c

--- labs/lab1/task2.c
+++ labs/lab1/task2.c
@@ -1,1 +1,18 @@
-
+int main()
+{
+    int a = 5;
+    int b = 2;
+    int c = a + b;
+    printf("c = %d\n",c);
+    
+    a = 8;
+    b = 2;
+    c = a + b;
+    printf("c = %d\n",c);
+    
+    a = 7;
+    b = 12;
+    c = a + b;
+    printf("c = %d\n",c);
+    return 0;
+}
