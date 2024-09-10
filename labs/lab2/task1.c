7ce3208873ca73acf6de85461bf882bca1e6a04f
lorgovdanil <lorgovdanil@gmail.com>
2024-09-10 10:48:47 +0500

    Update task1.c

--- labs/lab2/task1.c
+++ labs/lab2/task1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main()
+{
+    srand(time(NULL));
+    int col = rand()%10 + 1;
+    int mass[col];
+    
+    for (int i = 0; i < col; i++){
+        mass[i] = rand()%100;
+    }
+    
+    for (int i = 0; i < col; i++){
+        printf("[%d] = %d\n", i, mass[i]);
+    }
+    return 0;
+}
