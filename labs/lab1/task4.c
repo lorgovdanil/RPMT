1c4081183efc64238c923d4533cf4294f6c20995
lorgovdanil <lorgovdanil@gmail.com>
2024-09-10 09:55:27 +0500

    Update task4.c

--- labs/lab1/task4.c
+++ labs/lab1/task4.c
@@ -3,7 +3,16 @@ int main()
     float a;
     float b;
     float c;
+    float x;
     
+    printf("a = ");
     scanf("%f", &a);
+    printf("b = ");
+    scanf("%f", &b);
+    printf("c = ");
+    scanf("%f", &c);
+    
+    x = (c - b) / a;
+    printf("x=%f",x);
     return 0;
 }
