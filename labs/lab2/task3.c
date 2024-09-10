7b372f86c14b1fdc27086b250b34bfd7acb04039
lorgovdanil <lorgovdanil@gmail.com>
2024-09-10 10:56:44 +0500

    Update task3.c

--- labs/lab2/task3.c
+++ labs/lab2/task3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main()
+{
+    srand(time(NULL));
+    int col = rand()%10 + 1;
+    float mass[col];
+    
+    printf("col = %d\n", col);
+    for (int i = col - 1; i >= 0; i--){
+        mass[i] = (float)rand()/RAND_MAX;
+    }
+    
+    for (int i = 0; i < col; i++){
+        printf("[%d] = %f\n", i, mass[i]);
+    }
+    return 0;
+}
