47eb6a68bfccf262d6f41c4645a091dcb7afd56f
lorgovdanil <lorgovdanil@gmail.com>
2024-09-17 11:07:53 +0500

    Update task4.c

--- labs/lab2/task4.c
+++ labs/lab2/task4.c
@@ -5,7 +5,7 @@
 int main()
 {
     srand(time(NULL));
-    int col = rand()%10 + 1;
+    int col = rand()%10 + 3;
     float mass[col];
     float arg1;
     float arg2;
@@ -16,11 +16,28 @@ int main()
     
     printf("col = %d\n", col);
     for (int i = 0; i < col; i++){
-        mass[i] = (float)rand()/RAND_MAX;
+        mass[i] = arg1 + (float)rand()/RAND_MAX * (arg2 - arg1);
     }
+    
+    puz(mass, col);
     
     for (int i = 0; i < col; i++){
         printf("[%d] = %f\n", i, mass[i]);
     }
     return 0;
+}
+void puz(float x[], int s){
+    int stop;
+    stop = 0;
+    while(stop != 1){
+        stop = 1;
+        for(int i = 0; i < s - 1; i++){
+            if (x[i] > x[i + 1]){
+                x[i] = x[i] + x[i + 1];
+                x[i + 1] = x[i] - x[i + 1];
+                x[i] = x[i] - x[i + 1];
+                stop = 0;
+            }
+        }
+    }
 }
