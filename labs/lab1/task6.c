aa738f10e07f5560fd7891d3552e6e3232102d32
lorgovdanil <lorgovdanil@gmail.com>
2024-09-24 10:36:43 +0500

    Update task6.c

--- labs/lab1/task6.c
+++ labs/lab1/task6.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main()
+{
+    FILE *file1;
+    FILE *file2;
+    
+    char name[256];
+    scanf("%s", name);
+    file2 = fopen(name, "w");
+    
+    file1 = fopen("test.txt", "r");
+    char str[256];
+    for(int i = 0; i < 3; i = i + 1){
+        fgets(str, 256, file1);
+        fprintf(file2, "%s", str);
+    }
+
+    fclose(file1);
+    fclose(file2);
+}
