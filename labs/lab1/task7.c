b3667bba17316648967d68bb3f081390938be320
lorgovdanil <lorgovdanil@gmail.com>
2024-09-24 10:43:54 +0500

    Update task7.c

--- labs/lab1/task7.c
+++ labs/lab1/task7.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    FILE *file;
+    
+    char name[256];
+    scanf("%s", name);
+    file = fopen("test.txt", "a");
+    fprintf(file, "%s", name);
+    
+    fclose(file);
+}
