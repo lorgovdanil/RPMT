39b9a2c9ccd971dc4c1f88daf551c2aff58bbbe0
lorgovdanil <lorgovdanil@gmail.com>
2024-10-01 10:49:52 +0500

    Update task5.c

--- labs/lab1/task5.c
+++ labs/lab1/task5.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     FILE *file;
     
-    char name[256];
-    scanf("%s", name);
+    char *name = argv[1];
+    
     file = fopen(name, "r");
     char str[256];
     while(fgets(str, 256, file) != NULL){
