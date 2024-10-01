4dc84f49c8c2d401280f407934132bdc331d2f09
lorgovdanil <lorgovdanil@gmail.com>
2024-10-01 09:56:41 +0500

    Update task8.c

--- labs/lab1/task8.c
+++ labs/lab1/task8.c
@@ -7,7 +7,7 @@ int main()
     FILE *file2;
     
     file1 = fopen("test.txt", "a+");
-    file2 = fopen("vitaly.txt", "a+");
+    file2 = fopen("work.txt", "a+");
     
     char str[256];
     scanf("%s", str);
@@ -17,8 +17,19 @@ int main()
     while(fgets(pr, 256, file1) != NULL){
         fprintf(file2, "%s", pr);
     }
+    fclose(file2);
+    file2 = fopen("work.txt", "r");
     
+    fclose(file1);
+    file1 = fopen("test.txt", "w");
+    fclose(file1);
+    file1 = fopen("test.txt", "a");
+    
+    while(fgets(pr, 256, file2) != NULL){
+        fprintf(file1, "%s", pr);
+    }
     
     fclose(file2);
     fclose(file1);
+    remove("work.txt");
 }
