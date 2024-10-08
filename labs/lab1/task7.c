7a1d2576a7290c970eb6680ce03b808f2ec30594
lorgovdanil <lorgovdanil@gmail.com>
2024-10-08 10:02:21 +0500

    Update task7.c

--- labs/lab1/task7.c
+++ labs/lab1/task7.c
@@ -5,8 +5,9 @@ int main()
     FILE *file;
     
     char name[256];
-    scanf("%s", name);
+    scanf("%[^\n]%*c", name);
     file = fopen("test.txt", "a");
+    fputs("\n", file);
     fprintf(file, "%s", name);
     
     fclose(file);
