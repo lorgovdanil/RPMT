6a84066f062c11307bea8a7ae9ec2cf8b8fad112
lorgovdanil <lorgovdanil@gmail.com>
2024-10-15 10:35:25 +0500

    Update task4.c

--- labs/lab2/task4.c
+++ labs/lab2/task4.c
@@ -1,32 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include<malloc.h>
 
-int main()
-{
-    srand(time(NULL));
-    int col = rand()%10 + 3;
-    float mass[col];
-    float arg1;
-    float arg2;
-    printf("arg1 = ");
-    scanf("%f", &arg1);
-    printf("arg2 = ");
-    scanf("%f", &arg2);
-    
-    printf("col = %d\n", col);
-    for (int i = 0; i < col; i++){
-        mass[i] = arg1 + (float)rand()/RAND_MAX * (arg2 - arg1);
-    }
-    
-    Sort(mass, col);
-    
-    for (int i = 0; i < col; i++){
-        printf("[%d] = %f\n", i, mass[i]);
-    }
-    return 0;
-}
-void Sort(float x[], int s){
+void Sort(float* x, int s){
     int stop;
     stop = 0;
     while(stop != 1){
@@ -40,4 +17,42 @@ void Sort(float x[], int s){
             }
         }
     }
+}
+
+int main(int argc, char* argv[])
+{
+    
+    for(int i = 1; i < argc; i++) {
+        printf("Argument %s\n", argv[i]);
+    }
+    float col = strtol(argv[1], NULL, 10);
+    float arg1 = strtol(argv[2], NULL, 10);
+    float arg2 = strtol(argv[3], NULL, 10);
+    
+    srand(time(NULL));
+    
+    float* mass;
+    mass = (float*)malloc(sizeof(int) * col);
+    
+    for (int i = 0; i < col; i++){
+        mass[i] = arg1 + (float)rand()/RAND_MAX * (arg2 - arg1);
+    }
+    
+    for (int i = 0; i < col; i++){
+        printf("%f ", mass[i]);
+    }
+    printf("\n");
+    
+    Sort(mass, col);
+    FILE *file;
+    file = fopen("text.txt", "w");
+    
+    for (int i = 0; i < col; i++){
+        printf("%f ", mass[i]);
+        fprintf(file, "%f ", mass[i]);
+    }
+    fclose(file);
+    printf("\n");
+    
+    return 0;
 }
